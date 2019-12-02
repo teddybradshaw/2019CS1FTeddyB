3fc169b52beab4f2f46836295301f9a91de1db99
teddybradshaw <tbradshaw21@nscds.org>
2019-12-02 17:23:37 +0000

    hit a random letter

---
+++ Baseball project/Baseball.js
@@ -0,0 +1,164 @@
+var xTarget = Math.floor(Math.random() * $( window ).width());
+var yTarget = -Math.floor(Math.random() * $( window ).height());
+
+
+$('#baseball').click(
+    function(){
+     TweenMax.to('#baseball',.5,{x:-8,y:120,ease:Sine.easeOut});
+     TweenMax.to('#baseball',2,{x: xTarget,y: yTarget,delay:.4,ease:Sine.easeOut,onComplete:outcome});
+    }
+    
+);
+
+
+
+function outcome(){
+    var ball = $( "#baseball" );
+    var ballPosition = ball.position();
+    var ballX = ballPosition.left;
+    var ballY = ballPosition.top;
+    
+    var player = $('#exampleboxrightfield');
+    var playerPosition = player.position();
+    var playerX = playerPosition.left;
+    var playerY = playerPosition.top;
+    
+    
+    if( distance(ballX,ballY,playerX,playerY) < 100 ){
+        console.log ('out')
+        $('#fielder').fadeIn();
+    }  
+    else{
+           
+    }
+    
+    
+    console.log(distance(ballX,ballY, playerX, playerY));
+    
+} 
+
+function distance(x1,y1,x2,y2){
+    var sideA = Math.abs(x1-x2);
+    var sideB = Math.abs(y1-y2);
+  return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
+}
+
+/*function cf(){
+    var ball = $('#exampleboxcenterfield');
+    var ballPosition = ball.position();
+    var ballX = ballPosition.left;
+    var ballY = ballPosition.top;
+}
+
+function rf(){
+    var ball = $('#exampleboxrightfield');
+    var ballPosition = ball.position();
+    var ballX = ballPosition.left;
+    var ballY = ballPosition.top;
+}
+
+function thirdbase(){
+    var ball = $('#exampleboxthirdbase');
+    var ballPosition = ball.position();
+    var ballX = ballPosition.left;
+    var ballY = ballPosition.top;
+}
+
+function shortstop(){
+    var ball = $('#exampleboxshortstop');
+    var ballPosition = ball.position();
+    var ballX = ballPosition.left;
+    var ballY = ballPosition.top;
+}
+
+function secondbase(){
+    var ball = $('#exampleboxsecondbase');
+    var ballPosition = ball.position();
+    var ballX = ballPosition.left;
+    var ballY = ballPosition.top;
+}
+
+function firstbase(){
+    var ball = $('#exampleboxfirstbase');
+    var ballPosition = ball.position();
+    var ballX = ballPosition.left;
+    var ballY = ballPosition.top;
+}
+
+function pitcher(){
+    var ball = $('#exampleboxpitcher');
+    var ballPosition = ball.position();
+    var ballX = ballPosition.left;
+    var ballY = ballPosition.top;
+}*/
+
+$('#box1').click(
+    function(){
+        $('[player]').fadeOut();
+        $('#firstbase').fadeIn();
+    }
+);
+
+$('#box2').click(
+    function(){
+        $('[player]').fadeOut();
+        $('#secondbase').fadeIn();
+    }
+);
+
+$('#box3').click(
+    function(){
+        $('[player]').fadeOut();
+        $('#shortstop').fadeIn();
+    }
+);
+
+
+$('#box4').click(
+    function(){
+        $('[player]').fadeOut();
+        $('#thirdbase').fadeIn();
+    }
+);
+
+$('#box5').click(
+    function(){
+        $('[player]').fadeOut();
+        $('#pitcher').fadeIn();
+    }
+);
+
+$('#box6').click(
+    function(){
+        $('[player]').fadeOut();
+        $('#rightfield').fadeIn();
+    }
+);
+
+$('#box7').click(
+    function(){
+        $('[player]').fadeOut();
+        $('#centerfield').fadeIn();
+    }
+);
+
+$('#box8').click(
+    function(){
+         $('[player]').fadeOut();
+        $('#leftfield').fadeIn();
+    }
+);
+
+$('#box9').click(
+    function(){
+        $('[player]').fadeOut();
+        $('#catcher').fadeIn();
+    }
+);
+
+$('#box10').click(
+    function(){
+        $('[player]').fadeOut();
+        $('#hitter').fadeIn();
+    }
+);
