bd9405ddd595df83dc0ab0c35a8edee468e5a994
teddybradshaw <tbradshaw21@nscds.org>
2020-01-29 15:08:44 +0000

    finished challenge

--- Semester 2/challenge.js
+++ Semester 2/challenge.js
@@ -17,7 +17,7 @@ message = "Hello, how are you" ;
 console.log(noVowels(message))}
 
 
-    /*function noVowels(message){
+    /*function noVowels(message){
     var vowels = 'aeiou';
     var string = "";
     for(var i=0; i<message.length; i++){
@@ -25,24 +25,24 @@ console.log(noVowels(message))}
         if(vowels.indexOf(currentletter)<0)
             string += currentletter;
     }
-    return string
+    return string
 }
 //check if current letter is in vowels
 //if not in vowels, then copy to noVowelString
 
 message = "Hello, how are you?";
 console.log(noVowels(message));
-  */
- 
-function everyOther(message){
+  */
+
+function everyOther(message, x){
     var string = "";
 
-    for(var i=0; i<message.length; i+=4){
+    for(var i=0; i<message.length; i+=x){
         var fourthLetter = message.charAt(i);
         string += fourthLetter;
          }
       return string
     }
 
- message = "Hello, how are you?"
-console.log(everyOther(message));
+message = "Hello, how are you?"
+console.log(everyOther(message, 4));
