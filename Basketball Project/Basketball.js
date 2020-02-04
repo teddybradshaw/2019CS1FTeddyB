855bddaca1a95537953055c55e9b3af3fdaad820
teddybradshaw <tbradshaw21@nscds.org>
2020-02-04 19:16:22 +0000

    2/4/2020

--- Basketball Project/Basketball.js
+++ Basketball Project/Basketball.js
@@ -1,6 +1,15 @@
+var RayAllen = new Audio("Ray Allen.mp3")
+
 $('#box1').click(
     function(){
-        $('[player]').fadeOut();
-        $('#Kobe').fadeIn();
+        $('#KOBE').fadeIn();
     }
+);
+
+$('#basketball').click(
+    function(){
+        RayAllen.play();
+        TweenMax.to('#basketball',1.7,{x: 0,y: 200,delay:6,});
+    }
+    
 );
