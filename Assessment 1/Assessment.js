f47b23069994b9324aef3353c9993f81f3ec2878
teddybradshaw <tbradshaw21@nscds.org>
2020-03-09 18:24:02 +0000

    changes 3/9/2020

---
+++ Assessment 1/Assessment.js
@@ -0,0 +1,16 @@
+$("#box1").click(function(){
+    $("#rectangle").css("background-color", "yellow");
+});
+
+$("#box2").click(function(){
+    $("#rectangle").css("background-color", "blue");
+});
+
+$("box3").click(function(){
+    $("#rectangle").css("background-color", "brown");
+    $("#rectangle").text("4");
+});
+
+$("#box6").click(function(){
+    $("#rectangle").css("background-color", "yellow");
+});
